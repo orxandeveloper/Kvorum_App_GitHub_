d0949a761fa412b6f4e044cf5180a06c44f6b604
orxandeveloper <orxandeveloper@gmail.com>
2021-03-23 18:28:36 +0300

    nneew

--- Kvorum_App/Super_Disp/DispRequests.aspx.cs
+++ Kvorum_App/Super_Disp/DispRequests.aspx.cs
@@ -18,10 +18,26 @@ namespace Kvorum_App.Super_Disp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+        //    List<object> columns = new List<object>();
+        //    columns.Add( new { Rus = "Номер заявки", Eng = "MOBILE_NUMBER" } );
+        //    columns.Add( new { Rus = "Номер лицевого счета заявителя", Eng = "SCORE_ID" } );
+        //    columns.Add( new { Rus = "Заявитель", Eng = "FIRST_NAME" } );
+        //    columns.Add( new { Rus = "Номер телефона заявителя", Eng = "PHONE_NUMBER" } );
+        //    columns.Add( new { Rus = "Адрес объекта", Eng = "OBJECT_ADRESS" } );
+        //    columns.Add( new { Rus = "Номер помещения", Eng = "ROOM_NUMBER" } );
+        //    columns.Add( new { Rus = "Направление услуг", Eng = "DIRECTION_NAME" } );
+        //    columns.Add( new { Rus = "Группа услуг", Eng = "SERVICE_GROUP" } );
 
-            //  GiveExcel_for_Raport ( Mydb.ExecuteReadertoDataTable("select * from vw_RequestFor_Raport where MASTER_ID=878", new SqlParameter[] { }, CommandType.Text));
+            //var jsonForExample = new
+            //{
+
+            //    Eng = Eng_name,
+            //    Rus = Rus_name,
+            //};
+            //GiveExcel_for_Raport ( Mydb.ExecuteReadertoDataTable("select * from vw_RequestFor_Raport where MASTER_ID between 878 and 895"
+            //    , new SqlParameter[] { }, CommandType.Text), JsonConvert.SerializeObject(columns));
         }
-
+        
         public static string GiveExcel_for_Raport(DataTable dt,string Cnames)
         {
             string result = "";
@@ -38,20 +54,27 @@ namespace Kvorum_App.Super_Disp
                     int i = 0;
                     int k = 0;
                     dynamic Columns = JsonConvert.DeserializeObject(Cnames);
+                    List<string> list = new List<string>();
                     foreach (var column in  Columns)
                     {
-                        var ColName = column.ColumnName;
+                      //  var ColName = JsonConvert.DeserializeObject(column);
+                        var Eng_name = (string)column.Eng;
+                        var Rus_name = (string)column.Rus;
                         var letter = chars[k] + "1";
                         worksheet.Column(chars[k]).CellsUsed().SetDataType(XLDataType.Text);
-                        worksheet.Cell(letter).Value = ColName;
+                        worksheet.Cell(letter).Value = Rus_name;
+                        var jsonForExcel = new
+                        {
+                            CellAdres = chars[k],
+                            DbColumnName = Eng_name,
+                            Rus_cellHeader = Rus_name,
+                        };
+                        list.Add(JsonConvert.SerializeObject(jsonForExcel));
                         worksheet.Cell(letter).Style.Font.Bold = true;
 
                         i++;
                         k++;
-                        if (i == 15)
-                        {
-                            break;
-                        }
+                       
                     }
 
 
@@ -61,12 +84,66 @@ namespace Kvorum_App.Super_Disp
                     foreach (DataRow item in dt.Rows)
                     {
 
+                        for (int j = 0; j < list.Count; j++)
+                        {
+                            dynamic columnsAndCells = JsonConvert.DeserializeObject(list[j]);
+                            var eng_column = (string)columnsAndCells.DbColumnName;
+                            var RusHeader = (string)columnsAndCells.Rus_cellHeader;
+                            string CellAdres = (string)columnsAndCells.CellAdres;
+                            worksheet.Column( CellAdres).CellsUsed().SetDataType(XLDataType.Text);
+                            if (eng_column=="kol" || eng_column=="ed"||eng_column=="cost")
+                            {
+                                continue;
+                            }
+                            var dbValue = item[eng_column].ToString();
+                            if (eng_column== "EMERGENCY_TREATMENT")
+                            {
+                                dbValue = (dbValue=="False")?"": "Аварийная заявка";
+                            }
+                            if (eng_column== "REQUEST_SERVICES")
+                            {
+                                string[] dbValues = dbValue.Split('|');
+                                for (int f = 0; f < dbValues.Length; f++)
+                                {
+                                    dbValue = "";
+                                    var kol_cost_ed = dbValues[f].Substring(dbValues[f].IndexOf('(') + 1, (dbValues[f].IndexOf(')') - dbValues[f].IndexOf('(')) - 1);
+                                    dbValues[f] = dbValues[f].Substring(0, dbValues[f].IndexOf('('));
 
+                                    string[] kolcosted_array = kol_cost_ed.Split('^');
+                                    if (exitsInjson(list, "kol"))
+                                    {
+                                        dbValues[f] = dbValues[f] + " (" + kolcosted_array[0] + ")";
+
+                                    }
+                                    if (exitsInjson(list, "ed"))
+                                    {
+                                        dbValues[f] = dbValues[f] + " (" + kolcosted_array[1] + ")";
+                                    }
+                                    if (exitsInjson(list, "cost"))
+                                    {
+                                        dbValues[f] = dbValues[f] + " (" + kolcosted_array[2] + ")";
+                                    }
+                                    if (f==0)
+                                    {
+                                        dbValue = dbValues[f];
+                                    }
+                                    else
+                                    {
+                                        dbValue = dbValue+"," + dbValues[f];
+                                    }
+                                }
+                            }
+                            
+                            worksheet.Cell(CellAdres+cellNumber).Value =dbValue;
+                            var rngTable = worksheet.Range("A1:V" + cellNumber + "");
+                            rngTable.Style.Border.InsideBorder = XLBorderStyleValues.Thin;
+                            rngTable.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+
+                        }
+                        cellNumber++;
 
                     }
-                    var rngTable = worksheet.Range("A1:P" + cellNumber + "");
-                    rngTable.Style.Border.InsideBorder = XLBorderStyleValues.Thin;
-                    rngTable.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                    
                     worksheet.Columns().AdjustToContents();
                     workbook.SaveAs(strFilePath);
                     result = "Raport_" + DateTime.Now.ToString("dd.mm.yyyy") + "_" + guid + ".xlsx";
@@ -82,49 +159,70 @@ namespace Kvorum_App.Super_Disp
 
             return result;
         }
+
+        private static bool exitsInjson(List<string> list, string v)
+        {
+            var rvalue = false;
+            for (int j = 0; j < list.Count; j++)
+            {
+                dynamic columnsAndCells = JsonConvert.DeserializeObject(list[j]);
+                var eng_column = (string)columnsAndCells.DbColumnName;
+                var RusHeader = (string)columnsAndCells.Rus_cellHeader;
+                if (eng_column==v)
+                {
+                    rvalue = true;
+                }
+            }
+
+            return rvalue;
+       }
+
         [WebMethod]
         public static string RaportForRequest(string ColumnsName, string DataForRapor)
         {
-            dynamic Datas_json = JsonConvert.DeserializeObject(DataForRapor);
-            bool and_or = (bool)Datas_json.and_or;
-            string SCORES = (string)Datas_json.SCORES;
-            string INDIVIDUAL_ID = (string)Datas_json.INDIVIDUAL_ID;
-            string ROOM_NUMBER = (string)Datas_json.ROOM_NUMBER;
-            string SPESIALIST_ID = (string)Datas_json.SPESIALIST_ID;
-            string RESPONSIBLE_ID = (string)Datas_json.RESPONSIBLE_ID;
-            string STATUS_ID = (string)Datas_json.STATUS_ID;
-            string OBJECT_ID = (string)Datas_json.OBJECT_ID;
-            string SUPPLIER_GUID = (string)Datas_json.SUPPLIER_GUID;
-            string DIRECTION_GUID = (string)Datas_json.DIRECTION_GUID;
-            string SERVICE_GROUP_GUID = (string)Datas_json.SERVICE_GROUP_GUID;
-            string SERVICE_GUID = (string)Datas_json.SERVICE_GUID;
-            string PAYED = (string)Datas_json.PAYED;
-            string REQUEST_TYPE_ = (string)Datas_json.REQUEST_TYPE_;
-            string PAYMENT = (string)Datas_json.PAYMENT;
-            string EMERGENCY_TREATMENT = (string)Datas_json.EMERGENCY_TREATMENT;
-            string CR_DATE_from = (string)Datas_json.CR_DATE_from;
-            string CR_DATE_to = (string)Datas_json.CR_DATE_to;
-            GiveExcel_for_Raport(
-            Mydb.ExecuteReadertoDataTable("RaportForRequest", new SqlParameter[] {
-                new SqlParameter("@and_or",and_or),
-                new SqlParameter("@SCORES",SCORES),
-                new SqlParameter("@INDIVIDUAL_ID",INDIVIDUAL_ID),
-                new SqlParameter("@ROOM_NUMBER",ROOM_NUMBER),
-                new SqlParameter("@SPESIALIST_ID",SPESIALIST_ID),
-                new SqlParameter("@RESPONSIBLE_ID",RESPONSIBLE_ID),
-                new SqlParameter("@STATUS_ID",STATUS_ID),
-                new SqlParameter("@OBJECT_ID",OBJECT_ID),
-                new SqlParameter("@SUPPLIER_GUID",SUPPLIER_GUID),
-                new SqlParameter("@DIRECTION_GUID",DIRECTION_GUID),
-                new SqlParameter("@SERVICE_GROUP_GUID",SERVICE_GROUP_GUID),
-                new SqlParameter("@SERVICE_GUID",SERVICE_GUID),
-                new SqlParameter("@PAYED",PAYED),
-                new SqlParameter("@REQUEST_TYPE_",REQUEST_TYPE_),
-                new SqlParameter("@PAYMENT",PAYMENT),
-                new SqlParameter("@EMERGENCY_TREATMENT",EMERGENCY_TREATMENT),
-                new SqlParameter("@CR_DATE_from",CR_DATE_from),
-                new SqlParameter("@CR_DATE_to",CR_DATE_to)
-            }, CommandType.StoredProcedure),ColumnsName);
+            GiveExcel_for_Raport(Mydb.ExecuteReadertoDataTable("select * from vw_RequestFor_Raport where MASTER_ID between 878 and 880"
+              , new SqlParameter[] { }, CommandType.Text), ColumnsName);
+            // dynamic Datas_json = JsonConvert.DeserializeObject(DataForRapor);
+            //bool and_or = (bool)Datas_json.and_or;
+            //string SCORES = (string)Datas_json.SCORES;
+            //string INDIVIDUAL_ID = (string)Datas_json.INDIVIDUAL_ID;
+            //string ROOM_NUMBER = (string)Datas_json.ROOM_NUMBER;
+            //string SPESIALIST_ID = (string)Datas_json.SPESIALIST_ID;
+            //string RESPONSIBLE_ID = (string)Datas_json.RESPONSIBLE_ID;
+            //string STATUS_ID = (string)Datas_json.STATUS_ID;
+            //string OBJECT_ID = (string)Datas_json.OBJECT_ID;
+            //string SUPPLIER_GUID = (string)Datas_json.SUPPLIER_GUID;
+            //string DIRECTION_GUID = (string)Datas_json.DIRECTION_GUID;
+            //string SERVICE_GROUP_GUID = (string)Datas_json.SERVICE_GROUP_GUID;
+            //string SERVICE_GUID = (string)Datas_json.SERVICE_GUID;
+            //string PAYED = (string)Datas_json.PAYED;
+            //string REQUEST_TYPE_ = (string)Datas_json.REQUEST_TYPE_;
+            //string PAYMENT = (string)Datas_json.PAYMENT;
+            //string EMERGENCY_TREATMENT = (string)Datas_json.EMERGENCY_TREATMENT;
+            //string CR_DATE_from = (string)Datas_json.CR_DATE_from;
+            //string CR_DATE_to = (string)Datas_json.CR_DATE_to;
+
+            //GiveExcel_for_Raport(
+            //Mydb.ExecuteReadertoDataTable("RaportForRequest", new SqlParameter[] {
+            //    new SqlParameter("@and_or",and_or),
+            //    new SqlParameter("@SCORES",SCORES),
+            //    new SqlParameter("@INDIVIDUAL_ID",INDIVIDUAL_ID),
+            //    new SqlParameter("@ROOM_NUMBER",ROOM_NUMBER),
+            //    new SqlParameter("@SPESIALIST_ID",SPESIALIST_ID),
+            //    new SqlParameter("@RESPONSIBLE_ID",RESPONSIBLE_ID),
+            //    new SqlParameter("@STATUS_ID",STATUS_ID),
+            //    new SqlParameter("@OBJECT_ID",OBJECT_ID),
+            //    new SqlParameter("@SUPPLIER_GUID",SUPPLIER_GUID),
+            //    new SqlParameter("@DIRECTION_GUID",DIRECTION_GUID),
+            //    new SqlParameter("@SERVICE_GROUP_GUID",SERVICE_GROUP_GUID),
+            //    new SqlParameter("@SERVICE_GUID",SERVICE_GUID),
+            //    new SqlParameter("@PAYED",PAYED),
+            //    new SqlParameter("@REQUEST_TYPE_",REQUEST_TYPE_),
+            //    new SqlParameter("@PAYMENT",PAYMENT),
+            //    new SqlParameter("@EMERGENCY_TREATMENT",EMERGENCY_TREATMENT),
+            //    new SqlParameter("@CR_DATE_from",CR_DATE_from),
+            //    new SqlParameter("@CR_DATE_to",CR_DATE_to)
+            //}, CommandType.StoredProcedure),ColumnsName);
 
             return "";
         }
